01201d7f58fc0a2d49d268796fc8cde43b647f8e
AfraaAlobaid <afraa.obaid1@gmail.com>
2022-01-17 11:31:38 +1030

    Practiced advanced class methods

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,52 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    self.new_by_name(name)
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      return self.find_by_name(name)
+    else return self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(file_name)
+    delimiters = [" - ", ".mp3"]
+    data = file_name.split(Regexp.union(delimiters))
+    #data[1][".mp3"] = ""
+    song = self.create_by_name(data[1])
+    song.artist_name = data[0]
+    song
+  end
+
+  def self.create_from_filename(file_name)
+    self.new_from_filename(file_name)
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
